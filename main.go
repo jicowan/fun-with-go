5fc59fce960cbe5669a22134b885b9d9378b56af
jicowan <jicowan@hotmail.com>
2021-01-23 11:28:53 -0600

    added interface foobar

--- main.go
+++ main.go
@@ -6,11 +6,20 @@ type foo struct {
 	word string 
 }
 
+type foobar interface {
+	bar()
+}
+
 func main() {
 	fmt.Println("Hello world!")
 	f := foo {
 		word: "jkjkjkj",
 	}
+	f.bar()
+	submain(&f)
+}
+
+func submain(f foobar) {
 	f.bar()
 }
 
